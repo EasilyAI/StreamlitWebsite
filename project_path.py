c9de94afc97706bd09b18ca114680e4ff98ae554
EasilyAI <tools@easilyai.biz>
2024-08-07 16:27:43 +0300

    /

--- project_path.py
+++ project_path.py
@@ -4,9 +4,9 @@ module_path = os.path.abspath(os.path.join(__file__, *([os.pardir]*3)))
 if module_path not in sys.path:
     sys.path.append(module_path)
     sys.path.append(module_path+"\\")
-    sys.path.append(r'..\..\..')
-    sys.path.append(r'..\..')
-    sys.path.append(r'..')
+    sys.path.append('..\..\..')
+    sys.path.append('..\..')
+    sys.path.append('..')
 
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
