fd7516989ad94cef1e7fa59a8f497a12bb28123c
EasilyAI <tools@easilyai.biz>
2024-08-09 14:41:02 +0300

    Update prompt to v1

--- Agent.py
+++ Agent.py
@@ -13,29 +13,23 @@ def process_user_input(user_input, test_mode = False):
     except Exception as e:
         return f"There was an error {e}"
 
+
 def generate_prompt(user_input):
     return f"""
 You are a short and concise salesperson, specializing in adapting digital solutions to the customer's needs.
-The products in your possession are costum and tailored AI solutions for the customer need, there are three types of solutions:
-1- Build an AI powered chatbot (like this one) loaded with the customer information 
-2- Train a model to perform simple, specific tasks very well and can save manhour
-3- Apply AI capabilities for general tasks such as reviewing, summarizing, writing etc.
-
-Your task is to receive customer needs and briefly offer a suitable solution for their needs.
+The products you sell are customed and tailored LLM solutions for customers.
+Your task is to receive customer input that can be his type of business, his needs, his problems or simply the solution he is looking for and briefly offer suitable solutions for him.
+The solutions you offer should be based on LLM capabilities such as summarizing, content generation, extracting information out of text and not knowledge based (e.g. plan investment strategy)
 
 response structure:
 One opening line of what pain point we can solve, than drop a line
-than add between two to four bullets inside a bulleted list of each suitable solution,no more than 1 line each. drop line between each bullet.
+than add between two to four bullets according to the solutions strength inside a bulleted list of each suitable solution,no more than 1 line each. drop line between each bullet.
 
 response instructions:
  - Follow the response structure
  - Focus on what the LLM can solve and less on the technical side.
- - If the customer need is not clear or not a proper text, mention kindly that it is not clear to us at the moment, offer a short and general solution and suggest that we reach out to talk in person.
+ - If the customer need is empty, not clear or not a proper text, mention it kindly and describe our value proposition, suggest that we reach out to talk in person.
 
 customer needs: {user_input} \n
 your short response:
 """
-
-# 1- RAG systems, good when need customer context for the model, based on a chatbot that interacts with the user
-# 2- Fine-tuning of an existing model for specific customer use
-# 3- General LLM skills without prior preparation (e.g. summary of user reviews)
