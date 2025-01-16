1e1740bcbc56ed32ee015526e39fd81aa6015ca5
EasilyAI <omer@easilyai.biz>
2025-01-16 18:02:57 +0200

    remove scopes from authentication page

--- pages/4_InstaBot.py
+++ pages/4_InstaBot.py
@@ -11,8 +11,8 @@ load_default_page_config(layout="wide")
 st.title("Sign up for the InstaBot service")
 
 st.markdown(
-    '<a href="https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=1585703655622941&redirect_uri=https://rw7uq0spp4.execute-api.us-east-1.amazonaws.com/business-auth&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish" target="_blank" style="text-decoration:none;">'
-    # '<a href="https://rw7uq0spp4.execute-api.us-east-1.amazonaws.com/business-auth">'
+    '<a href="https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=1585703655622941&redirect_uri=https://rw7uq0spp4.execute-api.us-east-1.amazonaws.com/business-auth&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages" target="_blank" style="text-decoration:none;">'
+    # '<a href="https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=1585703655622941&redirect_uri=https://rw7uq0spp4.execute-api.us-east-1.amazonaws.com/business-auth&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish" target="_blank" style="text-decoration:none;">'
     '<button style="background-color:#4CAF50; color:white; border:none; padding:10px; font-size:16px; cursor:pointer;">'
     'Register'
     '</button>'
